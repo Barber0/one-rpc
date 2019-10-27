d77dc568d6c7b57abd3f88591f2598cf4025681b
simonfangzilin_i <simonfangzilin_i@didiglobal.com>
2019-10-27 21:55:51 +0800

    prepare to create a new protocol

--- protocol/common.go
+++ protocol/common.go
@@ -1,15 +1,17 @@
 package protocol
 
-type Logger interface {
-	Debug(msg...interface{})
-	Info(msg...interface{})
-	Warn(msg...interface{})
-	Error(msg...interface{})
+type TransportServer interface {
 
-	Debugf(tpl string, args...interface{})
-	Infof(tpl string, args...interface{})
-	Warnf(tpl string, args...interface{})
-	Errorf(tpl string, args...interface{})
 }
 
-const ONE_RPC_VERSION = 1
+type TransportClient interface {
+
+}
+
+type AppServerProtocol interface {
+
+}
+
+type AppClientProtocol interface {
+
+}
